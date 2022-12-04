9aa8726c3937e2350618e9ab96a737682918b51b
NE Official <106741077+Novo0007@users.noreply.github.com>
2022-12-04 14:18:45 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+//Audio Credits To Piyush
+
+var a = new Audio("https://dl.dropbox.com/s/e93f5kxsmtankf3/Best%20Happy%20Birthday%20To%20You%20_%20Happy%20Birthday%20Songs%20Remix%202020%28M4A_128K%29_1.m4a?dl=0");
+
+window.addEventListener('click',() => {
+a.play();
+});
+
+alert("Happy Birthday Shreya 😊 click on screen for music")
