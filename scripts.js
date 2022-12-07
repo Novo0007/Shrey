dec2d4bd486867df7a1725b75f7522a2cadcdbb6
NE Official <106741077+Novo0007@users.noreply.github.com>
2022-12-07 16:29:46 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Audio Credits To Piyush
 
-var a = new Audio("https://dl.dropbox.com/s/e93f5kxsmtankf3/Best%20Happy%20Birthday%20To%20You%20_%20Happy%20Birthday%20Songs%20Remix%202020%28M4A_128K%29_1.m4a?dl=0");
+var a = new Audio("Best Happy Birthday To You _ Happy Birthday Songs Remix 2020(M4A_128K)_1.m4a");
 
 window.addEventListener('click',() => {
 a.play();
