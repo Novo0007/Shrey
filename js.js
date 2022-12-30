886096e77dd2abb973855f7d3305b60aad3f1ee2
NE Official <106741077+Novo0007@users.noreply.github.com>
2022-12-30 15:13:04 +0530

    Update js.js

--- js.js
+++ js.js
@@ -8,25 +8,8 @@ a.play();
 
 alert(" Hii,-Happy Birthday Shreya🥳- click on screen for music")
 
-function apear(){
-    document.getElementById("HBD").style.display="block";
-}
+
 window.onload = function(){
     confetti.start(); 
 
 }
- function startTime() {
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        var s = today.getSeconds();
-        m = checkTime(m);
-        s = checkTime(s);
-        document.getElementById('txt').innerHTML =
-        h + ":" + m + ":" + s;
-        var t = setTimeout(startTime, 500);
-    }
-    function checkTime(i) {
-        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-        return i;
-    }
